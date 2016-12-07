4be15ba2d2ee3c267810c2d6f14df077ea7a050c
Igor Leao <ivpcl@cin.ufpe.br>
2016-12-07 13:17:34 -0300

    nova versao

--- js/app.js
+++ js/app.js
@@ -1,24 +1,3 @@
 (function() {
     let app = angular.module("AspectJML", []);
 })();
-
-let CHANGE_STYLE_TIMEOUT = 5000;
-
-function applySuccess() {
-    $("footer").css("background-color", "#689F38")
-    setTimeout(applyDefault, CHANGE_STYLE_TIMEOUT);
-}
-
-function applyDefault() {
-    $("footer").css("background-color", "#f7f7f7")
-}
-
-function applyError() {
-    $("footer").css("background-color", "#D32F2F")
-    setTimeout(applyDefault, CHANGE_STYLE_TIMEOUT);
-}
-
-function applyWarning() {
-    $("footer").css("background-color", "#FDD835")
-    setTimeout(applyDefault, CHANGE_STYLE_TIMEOUT);
-}
--- js/controllers/form.js
+++ js/controllers/form.js
@@ -12,17 +12,17 @@
 
     .directive('onKeyup', function() {
         return function(scope, elm, attrs) {
-          var allowedKeys = scope.$eval(attrs.keys);
-          elm.bind('keydown', function(evt) {
-            angular.forEach(allowedKeys, function(key) {
-              if (key == evt.which) {
-                 evt.preventDefault(); // Doesn't work at all
-                 window.stop(); // Works in all browsers but IE
-                 document.execCommand("Stop"); // Works in IE
-                 return false; // Don't even know why it's here. Does nothing.
-              }
+            var allowedKeys = scope.$eval(attrs.keys);
+            elm.bind('keydown', function(evt) {
+                angular.forEach(allowedKeys, function(key) {
+                    if (key == evt.which) {
+                        evt.preventDefault(); // Doesn't work at all
+                        window.stop(); // Works in all browsers but IE
+                        document.execCommand("Stop"); // Works in IE
+                        return false; // Don't even know why it's here. Does nothing.
+                    }
+                });
             });
-          });
         };
     })
 
@@ -39,6 +39,9 @@
         $scope.compiler = $scope.ASPECT_JML_COMPILER;
         $scope.compiler_outputs = [];
         $scope.current_compiler_output = null;
+        $scope.oneFile = false;
+        $scope.compilerOutput = "(compiler output will display here)"
+        $scope.programOutput = "(program output will display here)"
 
         openBuffer('Main.java');
         selectBuffer('Main.java');
@@ -46,11 +49,17 @@
         $scope.compile = function() {
             NProgress.start();
             $scope.compiling = true;
+            $scope.compilerOutput = "Compiling ..."
+            $scope.programOutput = "Waiting for the compile to run  ..."
+
             console.log("vamo compilar esse caraiii!");
             //let code = editor.getValue();
+
+            // TODO: ONEFILE!!!!!!!!!!!!!!!!!!!!!
             let request_data = {
                 "options": {
-                    "compiler": $scope.compiler
+                    "compiler": $scope.compiler,
+                    "one_file": $scope.oneFile
                 },
                 "code": []
             }
@@ -72,106 +81,105 @@
             $scope.selectTab($scope.curTab);
             console.log(request_data);
 
-            $http.post("http://localhost:3000/compile", request_data)
-                 .success(function(data, status) {
-                     $scope.compiler_outputs.push(data);
-                     $scope.current_compiler_output = data;
+            $http.post("http://54.203.4.64:3000/compile", request_data)
+            .success(function(data, status) {
+                $scope.compiler_outputs.push(data);
+                $scope.current_compiler_output = data;
 
-                     document.getElementById("console_output").value = data.completed ? data["program_output"] : data.errors
-                     if (data.completed) {
-                        applySuccess();
-                     } else {
-                         applyError();
-                     }
-                     NProgress.done();
-                     console.log(data);
-                 }).error(function (data, status, header, config) {
-                     console.log("DEU ERRRORRR")
-                     applyError();
-                     NProgress.done();
-                });
-                $scope.compiling = false;
+                $scope.programOutput = data.completed ? data["program_output"] : data.errors
+                $scope.compilerOutput = (data["compiler_output"] == "") ? ("Compiled at: "+ data["finished_at"]) : data["compiler_output"]
+
+                NProgress.done();
+                console.log(data);
+            }).error(function (data, status, header, config) {
+                console.log("DEU ERRRORRR")
+                //applyError();
+                NProgress.done();
+            });
+            $scope.compiling = false;
         }
 
-      $scope.addTab = function(tabName) {
-        $scope.tabs.push(tabName);
-        openBuffer(tabName);
-        $scope.selectTab(tabName);
-        $scope.newTabName = "";
-      }
-
-      $scope.removeTab = function(tab) {
-        var index = $scope.tabs.indexOf(tab);
-        if(index != -1) $scope.tabs.splice(index, 1);
-        if (tab == $scope.curTab) {
-          $scope.curTab = 'Main.java';
-          selectBuffer('Main.java');
+        $scope.addTab = function(tabName) {
+            $scope.tabs.push(tabName);
+            openBuffer(tabName);
+            $scope.selectTab(tabName);
+            $scope.newTabName = "";
         }
-        delete $scope.buffers[tab];
-      }
 
-      $scope.selectTab = function(tab) {
-        console.log(tab);
-        $scope.curTab = tab;
-        selectBuffer(tab);
-      }
+        $scope.removeTab = function(tab) {
+            var index = $scope.tabs.indexOf(tab);
+            if(index != -1) $scope.tabs.splice(index, 1);
+            if (tab == $scope.curTab) {
+                $scope.curTab = 'Main.java';
+                selectBuffer('Main.java');
+            }
+            delete $scope.buffers[tab];
+        }
 
-      $scope.isSelected = function(tab) {
-        return $scope.curTab === tab;
-      }
-
-      $scope.renameTab = function(tab) {
-        $scope.renTab = tab;
-      }
-
-      $scope.renameEnter = function(keyEvent, tab, inputValue) {
-        if (keyEvent.which === 13) {
+        $scope.selectTab = function(tab) {
             console.log(tab);
-            $scope.renTab = null;
-            if (typeof inputValue != 'undefined') {
-              console.log("Novo nome:")
-              var index = $scope.tabs.indexOf(tab);
-              if(index != -1) $scope.tabs[index] = inputValue;
-              renameBuffer(tab, inputValue);
-              $scope.curTab = inputValue;
+            $scope.curTab = tab;
+            selectBuffer(tab);
+        }
+
+        $scope.isSelected = function(tab) {
+            return $scope.curTab === tab;
+        }
+
+        $scope.renameTab = function(tab) {
+            $scope.renTab = tab;
+        }
+
+        $scope.renameEnter = function(keyEvent, tab, inputValue) {
+            if (keyEvent.which === 13) {
+                console.log(tab);
+                $scope.renTab = null;
+                if (typeof inputValue != 'undefined') {
+                    console.log("Novo nome:")
+                    var index = $scope.tabs.indexOf(tab);
+                    if(index != -1) $scope.tabs[index] = inputValue;
+                    renameBuffer(tab, inputValue);
+                    $scope.curTab = inputValue;
+                }
             }
         }
-      }
 
-      $scope.addCompilerOutputTab = function() {
-          console.log("AHIOOOIIIIII");
-          var tabName = "output:" + new Date().getTime();
-          $scope.addTab(tabName);
-          $scope.compiler_outputs.push(tabName);
-          var value = $scope.current_compiler_output["compiler_output"] || "Not compiled yet";
-          console.log(value);
-          editor.setValue(value);
-      }
+        $scope.addCompilerOutputTab = function() {
+            console.log("AHIOOOIIIIII");
+            var tabName = "output:" + new Date().getTime();
+            $scope.addTab(tabName);
+            $scope.compiler_outputs.push(tabName);
+            var value = $scope.current_compiler_output["compiler_output"] || "Not compiled yet";
+            console.log(value);
+            editor.setValue(value);
+        }
 
-      function renameBuffer(oldName, newName) {
-        var copyBuffer = $scope.buffers[oldName];
-        delete $scope.buffers[oldName]
-        $scope.buffers[newName] = copyBuffer
-      }
+        function renameBuffer(oldName, newName) {
+            var copyBuffer = $scope.buffers[oldName];
+            delete $scope.buffers[oldName]
+            $scope.buffers[newName] = copyBuffer
+        }
 
-      function selectBufferWithoutFocus(name) {
-          var buf = $scope.buffers[name];
-          if (buf.getEditor()) buf = buf.linkedDoc({sharedHist: true});
-          var old = editor.swapDoc(buf);
-          var linked = old.iterLinkedDocs(function(doc) {linked = doc;});
-          if (linked) {
-            for (var name in buffers) if ($scope.buffers[name] == old) $scope.buffers[name] = linked;
-            old.unlinkDoc(linked);
-          }
-      }
+        function selectBufferWithoutFocus(name) {
+            var buf = $scope.buffers[name];
+            if (buf.getEditor()) buf = buf.linkedDoc({sharedHist: true});
+            var old = editor.swapDoc(buf);
+            var linked = old.iterLinkedDocs(function(doc) {linked = doc;});
+            if (linked) {
+                for (var name in buffers) if ($scope.buffers[name] == old) $scope.buffers[name] = linked;
+                old.unlinkDoc(linked);
+            }
+        }
 
-      function selectBuffer(name) {
-        selectBufferWithoutFocus(name);
-        editor.focus();
-      }
+        function selectBuffer(name) {
+            selectBufferWithoutFocus(name);
+            editor.focus();
+        }
 
-      function openBuffer(name) {
-        $scope.buffers[name] = CodeMirror.Doc("", "javascript");
-      }
+        function openBuffer(name) {
+            var content = ""
+            if (name == "Main.java") content = document.getElementById("editor").innerHTML;
+            $scope.buffers[name] = CodeMirror.Doc(content, "text/x-java");
+        }
     });
 })();
